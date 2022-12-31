6252f0c1ccd228342855e5dd0400dbfcc304966a
TheOne-Byte <bryanchai18@gmail.com>
2022-12-31 21:29:15 +0700

    ini masih otak atik product ProductDetail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public function product_detail($id ){
-       $categories = Category::all();
-       $product = Product::find($id);
-       return view('ProductDetail', compact('Category', 'product'));
+    public function product_detail(){
+        $products = DB::table('products')->get();
+        $categories = DB::table('categories')->get();
+        return view('ProductDetail', ['products' => $products, 'categories' => $categories]);
     }
 
 
--- resources/views/ProductDetail.blade.php
+++ resources/views/ProductDetail.blade.php
@@ -3,23 +3,10 @@
 
 
 @section('headings')
-@foreach ( $products as $product )
-<div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col">
-      <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">{{ @$product->name }}</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-      </div>
-    </div>
-</div>
-
-@endforeach
+<h2>{{ $products->name }}</h2>
+{{-- <p>Category: {{ $products->category->name }}</p> --}}
+<p>{{ $products->description }}</p>
+<p>{{ $products->price }}</p>
+<img src="{{ $products->image }}">
 
 @endsection
