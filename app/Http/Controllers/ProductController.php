1b5ed69316465fc01074eea08c525621431d228a
TheOne-Byte <bryanchai18@gmail.com>
2022-12-31 21:33:49 +0700

    Otak Atik Blade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,10 +9,17 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
+<<<<<<< HEAD
     public function product_detail(){
         $products = DB::table('products')->get();
         $categories = DB::table('categories')->get();
         return view('ProductDetail', ['products' => $products, 'categories' => $categories]);
+=======
+    public function product_detail($index ){
+       $category = Category::all();
+       $product = Product::find($index);
+       return view('ProductDetail', compact('Category', 'product'));
+>>>>>>> 36cf007ef4f166f556170eb43457e532a72e0222
     }
 
 
